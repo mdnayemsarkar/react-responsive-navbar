b3623eb55decb9549bda9602792fe97bc07e6859
Nayem <nayeem3334@gmail.com.com>
2023-12-30 22:51:35 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import Component from "./ResposiveHeader";
 import Header from "./Header";
 function App() {
   return (
     <>
+    <Component />
       <Header />
       nayeem
       <div className="main flex justify-center text-green-500 font-bold text-3xl">
